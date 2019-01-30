d423bbe0301d4795e45d35e94a5a7a7de9454841
Denys Lozinskyi <42441410+DennyL@users.noreply.github.com>
2019-01-30 21:47:38 +0200

    Update renamer.py

--- renamer.py
+++ renamer.py
@@ -2,14 +2,14 @@ import os
 
 """
     NOTE! Source is a folder that containes files to be renamed with addition of "(translation)" word.
-    Dest is a folder that contains files, names of which are taken for renaming files in a source folder.
+    Dest is a folder that contains files, names of which are being taken for renaming files in a source folder.
     Files in both the folders share corresponding IDs, like:
-    21. Ivan Ivanov.pdf and 21. Иван Иванов.docx.
+    \dest\21. Ivan Ivanov.pdf and \source\21. Иван Иванов.docx.
     Here the first is an original file and the second is the translation of the first.
     An output of the script is to make a pair of those files with the same name,
     marking the one that contains a translation:
     So that "21. Иван Иванов.docx" became "21. Ivan Ivanov (translation).docx",
-    keeping an original extansion of the file.
+    keeping an original extension of the file.
 
     NOTE! For correct output of the script the files MUST have same IDs separated from the rest of a name by a dot.
 """
@@ -28,7 +28,7 @@ def main():
     '''
     for name in files_in_source:
         # initiating iteration over files in source folder
-        # picking id from every file
+        # picking id of a file
         fractals_of_name = name.split(".")
         name_id = fractals_of_name[0]
 
@@ -36,15 +36,18 @@ def main():
             # now, iterating over the files in dest folder to figure out,
             # if we have there a file with same id.
             if name_id == files_in_dest[i].split(".")[0]:
-                # when a pair is found, extract an extension of the file to be renamed
+                # if a pair is found, extract an extension of the file to be renamed
                 extension = fractals_of_name[-1]
                 # and building its new name based on a name from dest folder
-                # minus its extension (because we need to preserve the original one
+                # leaving aside its extension (because we need to preserve the original one)
                 new_file = os.path.join(path2source, files_in_dest[i].rpartition(".")[0] + " (translation)." + extension)
                 old_file = os.path.join(path2source, name)
-                print(old_file, "   --->   ", new_file)
                 os.rename(old_file, new_file)
+                print(old_file, "   --->   ", new_file)
                 break
+        else:
+            # if a file with such ID was not found
+            print("\nCannot find a pair for: " + name + "\n")
     print("Process is over")
 
 main()
